6f4f1e2d358eab77222d35efb9a605464506053c
Aditi Jaiswal <aditijaiswalalg999@gmail.com>
2025-08-13 17:03:35 +0530

    Initial commit

---
+++ lambda-app/index.js
@@ -0,0 +1,23 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+exports.handler = async (event) => {
+  console.log("Received event :", JSON.stringify(event, null, 2));
+  const params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key: 'hello.txt'
+  };
+
+  try {
+    const data = await s3.getObject(params).promise();
+    return {
+      statusCode: 200,
+      body: data.Body.toString('utf-8'),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: `Error fetching file: ${err.message}`,
+    };
+  }
+};
